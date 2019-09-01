b365da6213234e7d0744bd22ff3ebc3f2d9e3353
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-09-01 11:23:54 -0600

    add .net framework

---
+++ BlobStorageNF/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+
+namespace BlobStorageNF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount cuentaAlmacenamiento = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudBlobClient clienteBlob = cuentaAlmacenamiento.CreateCloudBlobClient();
+            CloudBlobContainer contenedor = clienteBlob.GetContainerReference("contenedorcodigo");
+            contenedor.CreateIfNotExists();
+            contenedor.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            CloudBlockBlob miBlob = contenedor.GetBlockBlobReference("foto3.jpg");
+
+            using (var fileStream = System.IO.File.OpenRead(@"C:\\RPADev\\cursos\\paas\\eiab.jpg"))
+            {
+                miBlob.UploadFromStream(fileStream);
+            }
+
+            Console.WriteLine("Tu contenedor esta listo y creado");
+            Console.ReadLine();
+        }
+    }
+}
