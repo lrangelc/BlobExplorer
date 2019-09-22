8506be14cf9ae59268f0eee50d62bc6875091e2b
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-09-21 21:39:34 -0600

    queue add

--- QueueExplorerNF/Program.cs
+++ QueueExplorerNF/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace QueueExplorerNF
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
+            CloudStorageAccount miCuenta = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("QueueConnectionString"));
+            CloudQueueClient clienteColas = miCuenta.CreateCloudQueueClient();
+            CloudQueue queue = clienteColas.GetQueueReference("mifiladeprocesos");
+            bool x = await queue.CreateIfNotExistsAsync();
+
+            for (int i = 0; i < 3; i++)
+            {
+                CloudQueueMessage mensaje = new CloudQueueMessage($"Operacion:{i}");
+                //queue.AddMessage(mensaje);
+                await queue.AddMessageAsync(mensaje);
+                Console.WriteLine($"El mensaje {i} ha sido publicado");
+            }
+            Console.WriteLine("Fin de Carga de Mensajes");
+            Console.ReadLine();
         }
     }
 }
